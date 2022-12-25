ad6bd792015c94be40123309c0dedfe82e4eb2b3
ebouiano <erik.b97@web.de>
2022-12-25 15:53:21 +0100

    add style to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
-@Component({
+
+
+ @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  styles: ['h1 { font-weight: normal; }'],
+  templateUrl: "./app.component.html",
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angularclient';
+
 }
+
+
