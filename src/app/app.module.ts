3b5131f950444a58bffbee944b9bdf47bc1bc5ba
ebouiano <erik.b97@web.de>
2022-12-26 10:41:36 +0100

    add grid layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RpsComponentComponent } from './rps-component/rps-component.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from "@angular/material/icon";
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     AppComponent,
