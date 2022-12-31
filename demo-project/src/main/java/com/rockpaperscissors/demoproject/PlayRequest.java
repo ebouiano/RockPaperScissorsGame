fe82b492f7eb593c146729c5ff865b3d58e4bc40
ebouiano <erik.b97@web.de>
2022-12-31 16:30:10 +0100

    fix documentation

--- demo-project/src/main/java/com/rockpaperscissors/demoproject/PlayRequest.java
+++ demo-project/src/main/java/com/rockpaperscissors/demoproject/PlayRequest.java
@@ -5,12 +5,12 @@ package com.rockpaperscissors.demoproject;
  * A class representing a request to play a game.
  */
 public class PlayRequest {
-     /** The name of the player making the request. */
+     /** The player symbol making the request. */
     private String player;
 
-     /**
-     * Returns the name of the player making the request.
-     * @return the name of the player making the request
+    /**
+     * Returns the player symbol making the request.
+     * @return the player symbol making the request
      */
     public String getPlayer() {
         return player;
