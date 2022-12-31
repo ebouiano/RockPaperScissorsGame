61e3509ecb92f781ba68d7a4a23a5103fc89aacc
ebouiano <erik.b97@web.de>
2022-12-31 15:32:50 +0100

    remove unnecessary cors configuration

--- demo-project/src/main/java/com/rockpaperscissors/demoproject/RockPaperScissorsApplication.java
+++ demo-project/src/main/java/com/rockpaperscissors/demoproject/RockPaperScissorsApplication.java
@@ -14,13 +14,6 @@ public class RockPaperScissorsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RockPaperScissorsApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:4200");
-		};
-	};
+	
 }
-}
+
