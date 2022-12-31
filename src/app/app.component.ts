09347fcea7a04eaf269dd83f573f069dc0ba438a
ebouiano <erik.b97@web.de>
2022-12-31 16:53:49 +0100

    typo fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,11 @@
 import { Component } from '@angular/core';
 
-
-
- @Component({
+@Component({
   selector: 'app-root',
   styles: ['h1 { font-weight: normal; }'],
-  templateUrl: "./app.component.html",
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
-
+  
 }
-
-
