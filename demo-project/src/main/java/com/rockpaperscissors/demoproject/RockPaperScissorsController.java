56217f9c7b5604977ca09d9c4c668d5d8e3ea856
ebouiano <erik.b97@web.de>
2023-01-17 20:41:17 +0100

    rm unused import

--- demo-project/src/main/java/com/rockpaperscissors/demoproject/RockPaperScissorsController.java
+++ demo-project/src/main/java/com/rockpaperscissors/demoproject/RockPaperScissorsController.java
@@ -1,6 +1,5 @@
 package com.rockpaperscissors.demoproject;
 
-import java.util.Random;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
